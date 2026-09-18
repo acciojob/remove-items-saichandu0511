181542e01dbe5907a195f7fe1208935b89bc734c
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-18 13:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 function removeColor(){
 	const select = document.getElementById("colorSelect");
-	select.remove(select.selectedIndex)
+	const selectedOption = select.option(select.selectedIndex);
+	select.removeChild(selectedOption);
 }
